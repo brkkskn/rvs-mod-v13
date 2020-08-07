3eff6bf49e95e91e2625e29f607d683ba9f9b7a5
Glitch (moderator-botu-revers) <none>
2020-08-07 15:16:51 +0000

    👩‍❤️‍💋‍👩🦀 Checkpoint ./bot.js:5998574/75

--- bot.js
+++ bot.js
@@ -143,7 +143,7 @@ client.on("guildMemberAdd", async (member) => {
     .setDescription(`<@${member.user.id}>**, Aramıza Hoşgeldin.**
 
      ${arrow} **Seninle Beraber ${member.guild.memberCount} Kişiyiz.**
-     ${arrow} **Teyit Olabilmeniz İçin ** '
+     ${arrow} **Teyit Olabilmeniz İçin -V.Confirmed- Odalarından Birine Geçiniz.** '
      ${arrow} **Kayıt Tarihi: ${moment.utc(member.user.createdAt).format('DD.MM.YY')}**
      ${arrow} **Güvenli Hesap!**
 
