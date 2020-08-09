d9e35030708f414570dc15b67eb482394d83e645
Glitch (revers-mod-bot) <none>
2020-08-09 12:16:01 +0000

    💖🚡 Checkpoint ./komutlar/idk.js:5998574/355 ./bot.js:5998574/2 ./komutlar/tag.js:5998574/25 ./komutlar/davet.js:5998574/3009

--- komutlar/tag.js
+++ komutlar/tag.js
@@ -1,25 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 exports.run = (client, message) => {
+  const arrow = client.emojis.get (`741028732784869406`) 
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Yapımcım:! ๖̶̶̶ۣۣۜۜ͜ζ͜͡xChairs#4713');
+    .setDescription(`**Iste Sunucu Tagımız** ${arrow}  \`そ\` `);
     message.channel.sendEmbed(ozelmesajkontrol) }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yapımcım'],
+  aliases: ['tagg'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'yapimcim',
-  description: 'Yapimcimi Gosterir.',
-  usage: 'yapimcim'
+  name: 'tag',
+  description: 'Tagı Gosterir.',
+  usage: '!tag'
 };
