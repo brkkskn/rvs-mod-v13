303b67399280ca96e1b1f3f37546eb7c325853f5
Glitch (revers-mod-bot) <none>
2020-08-27 16:48:22 +0000

    ⚽️🍰 Checkpoint ./komutlar/yetkililer.js:21238376/1822

--- komutlar/yetkililer.js
+++ komutlar/yetkililer.js
@@ -3,18 +3,35 @@ const client = new Discord.Client();
 
 exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
-    const ates = client.emojis.get (`747858093462782074`)
-    const ates1 = client.emojis.get (`747858109468508171`)
-    const ates2 = client.emojis.get (`747858124035195010`); 
+    const emoji = client.emojis.get (`747858093462782074`)
+    const emoji1 = client.emojis.get (`747858109468508171`)
+    const emoji2 = client.emojis.get (`747858124035195010`)
+    const emoji3 = client.emojis.get (`747858124035195010`)
+    const emoji4 = client.emojis.get (`747858124035195010`)
+    const emoji5 = client.emojis.get (`747858124035195010`)
+    const emoji6 = client.emojis.get (`747858124035195010`)
+    const emoji7 = client.emojis.get (`747858124035195010`); 
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription(`**${ates} Genel Sorumlu 'Rєvєrs ✦ DramaramaG#7777 **
+    .setDescription(`**${ates} Discord Genel Sorumlusu 'Rєvєrs ✦ DramaramaG#7777 **
 
     **${ates1} Team Sorumlusu Rєvєrs ✦ 𝓡𝓾𝓱 𝓐𝓭𝓪𝓶
 
-    ${ates2} Developer:Echoʳᵛˢ #0034**`);
+    Tag Alım Sorumlusu Rєvєrs ✦ 𝓡𝓾𝓱 𝓐𝓭𝓪𝓶
+
+    ${ates2} Chat Sorumlusu ✦ 𝓛𝓪 𝓿𝓲𝓮 𝓮𝓷 𝒸𝒾𝒶#4317**
+
+    Chat Sorumlusu Seneca [✦]#3333
+
+    Chat Sorumlusu ✦ᴸᴼᴷᴬᴸANESTEZİ#7320
+
+    Teyit Sorumlusu †  𝑭 𝑩 𝑰  ✦✦✦#4730
+
+    Teyit Sorumlusu Whiskey ✦#2222
+
+    Teyit Sorumlusu ✦Aeterna#0644`);
     message.channel.sendEmbed(ozelmesajkontrol) }
 };
 
